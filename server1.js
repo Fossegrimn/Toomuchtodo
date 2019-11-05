906eff2572efa0ad9c13e8d898e9942d089eab93
Fossegrimn <pettersorensen@hotmail.com>
2019-11-05 11:07:03 +0100

    Update server1.js

--- server1.js
+++ server1.js
@@ -5,10 +5,6 @@ const app = express();
 app.set('port', (process.env.PORT ||8080));
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(user);
-app.use(list);
-app.use(item);
-app.use(auth);
 
 app.listen(app.get('port'), function(){
 	console.log('server running', app.get('port'));
