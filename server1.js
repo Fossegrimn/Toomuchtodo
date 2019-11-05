28f199729c1df0682e532442a0405c5b9c9432e8
Fossegrimn <pettersorensen@hotmail.com>
2019-11-05 11:05:38 +0100

    Update server1.js

--- server1.js
+++ server1.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const db = require("./js/db.js");
-const user = require("./js/user.js");
-const list = require("./js/list.js");
-const item = require("./js/item.js");
-const auth = require("./js/auth.js");
 
 app.set('port', (process.env.PORT ||8080));
 app.use(express.static('public'));
