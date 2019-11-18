97a5eabddb5254b8e9d7d205ebcf492f19e2185b
Fossegrimn <pettersorensen@hotmail.com>
2019-11-18 16:11:10 +0100

    added func: update list and items

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//--------------------------- Version 1.4 ---------------------------------------
+//--------------------------- Version 2.0 ---------------------------------------
 
 const express = require('express');
 const cors = require('cors'); //when the clients aren't on the server
